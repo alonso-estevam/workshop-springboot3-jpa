5b3430b8fd3f0278db7f48217dc4552a4fe40463
alonso-estevam <alonso_estevam@outlook.com>
2024-03-09 18:37:31 -0300

    H2 database, test profile, JPA

--- src/main/java/com/alonsoestevam/springcourse/entities/User.java
+++ src/main/java/com/alonsoestevam/springcourse/entities/User.java
@@ -1,11 +1,17 @@
 package com.alonsoestevam.springcourse.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user") // "user" é uma palavra reservada do H2, então renomeamos para "tb_user" para não dar conflito
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
