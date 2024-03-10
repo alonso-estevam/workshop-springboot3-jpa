9a61e2b99afda9eeb66cb6a9b74a022f04e838c1
alonso-estevam <alonso_estevam@outlook.com>
2024-03-09 22:17:50 -0300

    Service layer, component registration

--- src/main/java/com/alonsoestevam/springcourse/controllers/UserController.java
+++ src/main/java/com/alonsoestevam/springcourse/controllers/UserController.java
@@ -1,18 +1,32 @@
 package com.alonsoestevam.springcourse.controllers;
 
 import com.alonsoestevam.springcourse.entities.User;
+import com.alonsoestevam.springcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> getUsers(){
-        User u = new User(1L, "Aline", "liny@email.com", "33841549", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
--- src/main/java/com/alonsoestevam/springcourse/repositories/UserRepository.java
+++ src/main/java/com/alonsoestevam/springcourse/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.alonsoestevam.springcourse.repositories;
 import com.alonsoestevam.springcourse.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// aqui o @Repository é opcional
 public interface UserRepository extends JpaRepository<User, Long> {
 }
