7211608f0cd336854239f29bce20cf5babb08ec2
alonso-estevam <alonso_estevam@outlook.com>
2024-03-14 11:10:31 -0300

    Exception handling - delete

--- src/main/java/com/alonsoestevam/springcourse/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/alonsoestevam/springcourse/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.alonsoestevam.springcourse.controllers.exceptions;
 
+import com.alonsoestevam.springcourse.services.exceptions.DatabaseException;
 import com.alonsoestevam.springcourse.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,16 @@ public class ResourceExceptionHandler {
                                                           HttpServletRequest request){
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(
+                Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException e,
+                                                          HttpServletRequest request){
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(
                 Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
